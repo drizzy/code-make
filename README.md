bf2ed74670847f0d14fc296e35f5d35cd0bf3165
Drizzy <hello@drizzy.dev>
2022-10-04 18:22:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Code Make
+
+[![Marketplace Version](https://vsmarketplacebadge.apphb.com/version-short/drizzy.code-make.svg)](https://marketplace.visualstudio.com/items?itemName=drizzy.code-make)
+[![Downloads](https://vsmarketplacebadge.apphb.com/downloads-short/drizzy.code-make.svg)](https://marketplace.visualstudio.com/items?itemName=drizzy.code-make)
+[![Rating](https://vsmarketplacebadge.apphb.com/rating-short/drizzy.code-make.svg)](https://marketplace.visualstudio.com/items?itemName=drizzy.code-make)
+
+## Features
+
+Create projects in c++ Compile and run the program directly from the vscode interface just by pressing an icon.
+
+## Requirements
+
+* You have to want `make` installed on your computer
+* If you are on linux you must install `gcc `
+* If you are on window you must install `mingw`
+* If you are on mac os you must install `clang`
+
+## How to use
+
+After installing the extension I'm sure you saw a `+` symbol in the bottom left corner, if you click on it it will create a C++ project structure for you.
+To compile just click the gear wheel, the play symbol to run the program, the trash can to delete files from the `bin` and `obj` directories, and x is to delete both directories.
+
+## Keybindings
+| Linux  | Windows | Mac | Description  |
+| ------------ | ------------ | ------------ | ------------ |
+| ctrl+alt+f   | ctrl+alt+f | ctrl+alt+f   | Make Create Folder |
+| ctrl+alt+b   | ctrl+alt+b | ctrl+alt+b   | Make Build Folder  |
+| ctrl+alt+r   | ctrl+alt+r | ctrl+alt+r   | Make Run Folder    |
+| ctrl+alt+c   | ctrl+alt+c | ctrl+alt+c   | Make Clean Folder  |
+| ctrl+alt+d   | ctrl+alt+d | ctrl+alt+d   | Make Delete Folder |
+
+## Release Notes
+
+Refer to [CHANGELOG](CHANGELOG.md)
---
+++ src/items.js
@@ -0,0 +1,112 @@
+const vscode    = require('vscode');
+const os        = require('os');
+const fs        = require('fs');
+const events    = require('events');
+const path      = require('path');
+const workspace = vscode.workspace;
+const window    = vscode.window;
+const commands  = vscode.commands;
+
+/* Creating a new event emitter. */
+let eventEmitter = events.EventEmitter;
+
+/* Creating a new event emitter. */
+let event = new eventEmitter();
+
+/* Getting the path of the workspace folder. */
+const folderPath = workspace.workspaceFolders[0].uri
+      .toString()
+      .split(':')[1];
+
+
+/* Creating a new status bar item for each of the commands. */
+let items = {
+  work:    window.createStatusBarItem(vscode.StatusBarAlignment.Left),
+  create:  window.createStatusBarItem(vscode.StatusBarAlignment.Left),
+  build:   window.createStatusBarItem(vscode.StatusBarAlignment.Left),
+  run:     window.createStatusBarItem(vscode.StatusBarAlignment.Left),
+  clear:   window.createStatusBarItem(vscode.StatusBarAlignment.Left),
+  destroy: window.createStatusBarItem(vscode.StatusBarAlignment.Left),
+}
+
+/**
+ * It updates the status bar items based on the current workspace
+ */
+let updateStatusBar = function () {
+
+  items.work.text = `$(folder-active) ${workspace.name}`;
+  items.work.command = null;
+  items.work.color = '#FF79C6';
+
+  items.create.text = `$(add)`;
+  items.create.command = 'code-make-create.run';
+  items.create.color = '#FF79C6';
+
+  items.build.text = `$(gear)`;
+  items.build.command = 'code-make-build.run';
+  items.build.color   = '#5F6E9F';
+
+  items.run.text = `$(debug-start)`;
+  items.run.command = 'code-make.run';
+  items.run.color = '#89D185' 
+
+  items.clear.text = `$(trash)`;
+  items.clear.command = 'code-make-clean.run';
+  items.clear.color = '#A7E0FB';
+    
+  items.destroy.text = `$(close)`;
+  items.destroy.command = 'code-make-destroy.run';
+  items.destroy.color = '#ED836E';
+
+  if (fs.existsSync(path.join(folderPath, 'src')) && fs.existsSync(path.join(folderPath, 'include')) && fs.existsSync(path.join(folderPath, 'Makefile'))){
+
+    items.create.hide();
+
+    items.work.show();
+
+    items.build.show();
+  
+    items.run.show();
+  
+    items.clear.show();
+  
+    items.destroy.show();
+
+  
+  } else {
+    
+    items.create.show();
+    
+    items.work.hide();
+
+    items.build.hide();
+  
+    items.run.hide();
+  
+    items.clear.hide();
+  
+    items.destroy.hide();
+  
+  }
+
+}
+
+/* Listening for an event called 'update' and when it is triggered it will run the function
+`updateStatusBar`. */
+event.on('update', updateStatusBar);
+
+/* Updating the status bar every 500 milliseconds. */
+setInterval( () => {
+  event.emit('update', Date.now());
+  },500
+);
+
+module.exports = {
+  os,
+  fs,
+  path,
+  workspace,
+  folderPath,
+  window,
+  commands
+}
